4e96053b3e31937dff64021d4ecd6331142054af
Karnakar <karnakar5511@gmail.com>
2025-03-13 15:38:33 +0530

    added deployed db

--- Connectdb.js
+++ Connectdb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Connect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/eventDB", {
+        await mongoose.connect("mongodb+srv://admin:admin@lab-mern-exam.avhan.mongodb.net/eventDB", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
